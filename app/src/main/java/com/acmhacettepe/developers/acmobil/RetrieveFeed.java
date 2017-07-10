8f4095db71b75395724aaaf3028e23faa36ec004
gokberksahin <b21627615@cs.hacettepe.edu.tr>
2017-07-10 13:04:52 +0300

    RetrieveFeed class added.

--- app/src/main/java/com/acmhacettepe/developers/acmobil/RetrieveFeed.java
+++ app/src/main/java/com/acmhacettepe/developers/acmobil/RetrieveFeed.java
@@ -1,8 +1,96 @@
 package com.acmhacettepe.developers.acmobil;
 
+import android.os.AsyncTask;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
 /**
  * Created by Gokberk on 7/4/2017.
  */
 
-public class RetrieveFeed {
+public class RetrieveFeed extends AsyncTask {
+
+    URL url;
+    ArrayList<String> headlines = new ArrayList();
+    ArrayList<String> links = new ArrayList();
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        // Initializing instance variables
+
+
+        try {
+            url = new URL("http://feeds.bbci.co.uk/news/rss.xml?edition=uk");
+
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(false);
+            XmlPullParser xpp = factory.newPullParser();
+
+            // We will get the XML from an input stream
+            xpp.setInput(getInputStream(url), "UTF_8");
+
+        /* We will parse the XML content looking for the "<title>" tag which appears inside the "<item>" tag.
+         * However, we should take in consideration that the rss feed name also is enclosed in a "<title>" tag.
+         * As we know, every feed begins with these lines: "<channel><title>Feed_Name</title>...."
+         * so we should skip the "<title>" tag which is a child of "<channel>" tag,
+         * and take in consideration only "<title>" tag which is a child of "<item>"
+         *
+         * In order to achieve this, we will make use of a boolean variable.
+         */
+            boolean insideItem = false;
+
+            // Returns the type of current event: START_TAG, END_TAG, etc..
+            int eventType = xpp.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if (eventType == XmlPullParser.START_TAG) {
+
+                    //TODO: "item", "title" ve "link" stringlerini uygun olanlara degistir ve ekleme/cıkarma yap.
+
+                    if (xpp.getName().equalsIgnoreCase("item")) {
+                        insideItem = true;
+                    } else if (xpp.getName().equalsIgnoreCase("title")) {
+                        if (insideItem)
+                            headlines.add(xpp.nextText()); //extract the headline
+                    } else if (xpp.getName().equalsIgnoreCase("link")) {
+                        if (insideItem)
+                            links.add(xpp.nextText()); //extract the link of article
+                    }
+                } else if (eventType == XmlPullParser.END_TAG && xpp.getName().equalsIgnoreCase("item")) {
+                    insideItem = false;
+                }
+
+                eventType = xpp.next(); //move to next element
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return headlines;
+    }
+
+
+    public InputStream getInputStream(URL url) {
+        try {
+            return url.openConnection().getInputStream();
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public ArrayList<String> heads()
+    {
+        return headlines;
+    }
 }
--- app/src/main/java/com/acmhacettepe/developers/acmobil/YemekListesi.java
+++ app/src/main/java/com/acmhacettepe/developers/acmobil/YemekListesi.java
@@ -7,7 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
+import java.util.ArrayList;
 
 
 public class YemekListesi extends Fragment {
