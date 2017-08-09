cb241abeb3f6427a5ddf62c02a16d47167ca47a5
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2017-08-09 14:00:15 +0300

    Added push notifications and added more locations.(fix)

---
+++ app/src/main/java/com/acmhacettepe/developers/acmobil/MyFirebaseMessagingService.java
@@ -0,0 +1,44 @@
+package com.acmhacettepe.developers.acmobil;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by Oguz on 8/9/2017.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "FCM Service";
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO: Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated.
+        System.out.println(remoteMessage.getFrom()); //ID
+        System.out.println(remoteMessage.getNotification().getBody()); //Message
+
+        NotificationCompat.Builder builder =
+                (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.send)
+                        .setContentTitle("ACMobil")
+                        .setContentText(remoteMessage.getNotification().getBody());
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        // Add as notification
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
+    }
+}
---
+++ app/src/main/java/com/acmhacettepe/developers/acmobil/YemekActivity.java
@@ -0,0 +1,13 @@
+package com.acmhacettepe.developers.acmobil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class YemekActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yemek);
+    }
+}
