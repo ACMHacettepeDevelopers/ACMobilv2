55fb277bf465d2995b22b49eaa7b5ae0f262fe16
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2017-08-18 19:47:35 +0300

    Last touches2

--- app/src/main/java/com/acmhacettepe/developers/acmobil/ResetPasswordActivity.java
+++ app/src/main/java/com/acmhacettepe/developers/acmobil/ResetPasswordActivity.java
@@ -67,7 +67,7 @@ public class ResetPasswordActivity extends AppCompatActivity {
                 String email = inputEmail.getText().toString().trim();
 
                 if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(getApplication(), "Enter your registered email id", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplication(), "Üye olduğunuz maili giriniz", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
@@ -77,9 +77,9 @@ public class ResetPasswordActivity extends AppCompatActivity {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
                                 if (task.isSuccessful()) {
-                                    Toast.makeText(ResetPasswordActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(ResetPasswordActivity.this, "Mail adresinize şifrenizi sıfırlamanız için bilgileri yolladık.", Toast.LENGTH_SHORT).show();
                                 } else {
-                                    Toast.makeText(ResetPasswordActivity.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(ResetPasswordActivity.this, "Bir şey oldu.", Toast.LENGTH_SHORT).show();
                                 }
 
                                 progressBar.setVisibility(View.GONE);
