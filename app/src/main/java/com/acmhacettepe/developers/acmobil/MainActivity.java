dd86806d91592972ff3db98eedeaece76fb33ea0
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2017-08-18 16:39:01 +0300

    Buggy commit for testing

--- app/src/main/java/com/acmhacettepe/developers/acmobil/MainActivity.java
+++ app/src/main/java/com/acmhacettepe/developers/acmobil/MainActivity.java
@@ -205,9 +205,7 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
-        } else if (id == R.id.action_info){
-            Intent intent = new Intent(this, Info.class);
+            Intent intent = new Intent(this, InfoActivity.class);
             startActivity(intent);
         }
 
