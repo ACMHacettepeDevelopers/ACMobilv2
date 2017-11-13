400a224c87951a8ebfd90a62908ccfe6401a7c4f
gokberk <b21627615@cs.hacettepe.edu.tr>
2017-11-13 23:06:38 +0300

    Qr Code Scanner Feature added but not working on 7.0

---
+++ app/src/main/java/com/acmhacettepe/developers/acmobil/QrScannerActivity.java
@@ -0,0 +1,13 @@
+package com.acmhacettepe.developers.acmobil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QrScannerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_scanner);
+    }
+}
