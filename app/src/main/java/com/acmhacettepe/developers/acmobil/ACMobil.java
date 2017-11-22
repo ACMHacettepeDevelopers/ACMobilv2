d914fccf00fc8194486717882721c9600951f93a
gokberk <b21627615@cs.hacettepe.edu.tr>
2017-11-22 21:14:48 +0300

    Forgot to switch off the debugging feature of Picasso.

--- app/src/main/java/com/acmhacettepe/developers/acmobil/ACMobil.java
+++ app/src/main/java/com/acmhacettepe/developers/acmobil/ACMobil.java
@@ -20,7 +20,7 @@ public class ACMobil extends Application {
         Picasso.Builder builder = new Picasso.Builder(this);
         builder.downloader(new OkHttpDownloader(this, Integer.MAX_VALUE));
         Picasso built = builder.build();
-        built.setIndicatorsEnabled(true);
+        built.setIndicatorsEnabled(false);
         built.setLoggingEnabled(true);
         Picasso.setSingletonInstance(built);
     }
