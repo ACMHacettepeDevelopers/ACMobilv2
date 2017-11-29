845e2580ec73080cc89588f46ba14d6c894b5f3f
gokberk <b21627615@cs.hacettepe.edu.tr>
2017-11-29 20:28:08 +0300

    Some refactoring on KoorMemberListActivity

--- app/src/main/java/com/acmhacettepe/developers/acmobil/KoorMemberListActivity.java
+++ app/src/main/java/com/acmhacettepe/developers/acmobil/KoorMemberListActivity.java
@@ -13,7 +13,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -24,7 +23,6 @@ public class KoorMemberListActivity extends AppCompatActivity {
     private Spinner mCoorSpinner;
     private ListView mNamesListView;
     private ArrayList<String> names = new ArrayList<>();
-    
 
 
     @Override
@@ -32,39 +30,44 @@ public class KoorMemberListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_koor_member_list);
 
+        // Setting up database root.
         addedUsers = FirebaseDatabase.getInstance().getReference().child("AddedUsers");
 
         mCoorSpinner = (Spinner) findViewById(R.id.coors_spinner);
 
         mNamesListView = (ListView) findViewById(R.id.listView_names);
 
-
+        // Creating and setting adapter for spinner.
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(KoorMemberListActivity.this,
                 android.R.layout.simple_list_item_1,
                 getResources().getStringArray(R.array.coor_names));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mCoorSpinner.setAdapter(myAdapter);
 
-
-
-
+        // Listens and runs the code below whenever selected item is changed in spinner.
         mCoorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 names = new ArrayList<>();
+                // Getting selected from spinner item for comparison.
                 final String coor = mCoorSpinner.getSelectedItem().toString();
-                System.out.println(coor);
 
+                // Reading the child "AddedUsers".
                 addedUsers.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Key is student Number and values are again a Hash Map.
                         HashMap<String, HashMap<String,String>> usersMap =
                                 ((HashMap<String,HashMap<String,String>>) dataSnapshot.getValue());
                         Iterator itrOuter = usersMap.values().iterator();
-                        while (itrOuter.hasNext()) {
 
+                        while (itrOuter.hasNext()) {
+                            /* Each Hash Map is storing the values of a user. In this case
+                             * the values below the student number like acmNum, username etc. */
                             HashMap<String,String> innerMap = (HashMap<String,String>) itrOuter.next();
 
+                            /* Checking the if such a key named "Koordinatorluk" and "name" exists or not
+                             * to avoid NullPointerException. Finally adding the name to list. */
                             if (innerMap.get("Koordinatorluk") != null) {
                                 if (innerMap.get("Koordinatorluk").equals(coor)) {
                                     if (innerMap.get("name") != null) {
@@ -73,25 +76,26 @@ public class KoorMemberListActivity extends AppCompatActivity {
                                 }
                             }
                         }
-                        final ArrayAdapter<String> nameAdapter = new ArrayAdapter<String>(KoorMemberListActivity.this,
+                        // Creating the adapter for listView and setting it.
+                        final ArrayAdapter<String> nameAdapter = new ArrayAdapter<>
+                                (KoorMemberListActivity.this,
                                 android.R.layout.simple_list_item_1, names);
                         mNamesListView.setAdapter(nameAdapter);
-                        System.out.println(nameAdapter.getContext());
                     }
 
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
                         Toast.makeText(KoorMemberListActivity.this, "Some error occured!",
                                 Toast.LENGTH_LONG).show();
-
                     }
                 });
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(KoorMemberListActivity.this, "Please select an option!",
+                        Toast.LENGTH_LONG).show();
 
             }
         });
-
     }
 }
